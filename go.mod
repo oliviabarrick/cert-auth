3a2e9ad10c32b8f75b52ee1963442f9283c623de
Justin Barrick <justin.m.barrick@gmail.com>
2019-06-23 14:32:36 -0700

    initial implementation

---
+++ go.mod
@@ -0,0 +1,36 @@
+module github.com/justinbarrick/cert-auth
+
+go 1.12
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-logr/logr v0.1.0 // indirect
+	github.com/go-logr/zapr v0.1.1 // indirect
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/google/gofuzz v1.0.0 // indirect
+	github.com/google/uuid v1.1.1
+	github.com/googleapis/gnostic v0.3.0 // indirect
+	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/imdario/mergo v0.3.7 // indirect
+	github.com/json-iterator/go v1.1.6 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/spf13/pflag v1.0.3 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20190621222207-cc06ce4a13d4 // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.2.2
+	k8s.io/api v0.0.0-20181121191454-a61488babbd6
+	k8s.io/apimachinery v0.0.0-20190211022232-e355a776c090
+	k8s.io/client-go v2.0.0-alpha.0.0.20190115175254-86dbf26d38ed+incompatible
+	k8s.io/klog v0.3.3 // indirect
+	sigs.k8s.io/controller-runtime v0.1.10
+	sigs.k8s.io/yaml v1.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"net/http"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+	certificatesv1beta1 "k8s.io/api/certificates/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/serializer/json"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	certificates "k8s.io/client-go/kubernetes/typed/certificates/v1beta1"
+	api "k8s.io/client-go/tools/clientcmd/api/v1"
+	csrutils "k8s.io/client-go/util/certificate/csr"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+)
+
+func generateCSR(names []string) ([]byte, []byte, error) {
+	key, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to genarate private keys, error: %s", err)
+	}
+
+	csrCertificate, err := x509.CreateCertificateRequest(rand.Reader, &x509.CertificateRequest{
+		Subject: pkix.Name{
+			CommonName: names[0],
+		},
+		DNSNames:           names,
+		SignatureAlgorithm: x509.SHA512WithRSA,
+	}, key)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return pem.EncodeToMemory(&pem.Block{
+			Type: "PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key),
+		}), pem.EncodeToMemory(&pem.Block{
+			Type: "CERTIFICATE REQUEST", Bytes: csrCertificate,
+		}), nil
+}
+
+func generateKubernetesCSR(name string, csr []byte) (*certificatesv1beta1.CertificateSigningRequest, error) {
+	uuid, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	return &certificatesv1beta1.CertificateSigningRequest{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: fmt.Sprintf("%s-%s", name, uuid.String()),
+		},
+		Spec: certificatesv1beta1.CertificateSigningRequestSpec{
+			Request: csr,
+			Usages: []certificatesv1beta1.KeyUsage{
+				certificatesv1beta1.UsageDigitalSignature,
+				certificatesv1beta1.UsageKeyEncipherment,
+				certificatesv1beta1.UsageClientAuth,
+			},
+		},
+	}, nil
+}
+
+func approveKubernetesCSR(csr *certificatesv1beta1.CertificateSigningRequest) {
+	csr.Status = certificatesv1beta1.CertificateSigningRequestStatus{
+		Conditions: []certificatesv1beta1.CertificateSigningRequestCondition{
+			certificatesv1beta1.CertificateSigningRequestCondition{
+				Type:           certificatesv1beta1.CertificateApproved,
+				Reason:         "CertAuthApprove",
+				Message:        "cert-auth automated provisioning",
+				LastUpdateTime: metav1.Now(),
+			},
+		},
+	}
+}
+
+func getCertificate(csrAPI certificates.CertificateSigningRequestInterface, name string, csr []byte) ([]byte, error) {
+	kubernetesCSR, err := generateKubernetesCSR(name, csr)
+	if err != nil {
+		return nil, err
+	}
+
+	createdCSR, err := csrAPI.Create(kubernetesCSR)
+	if err != nil {
+		return nil, err
+	}
+
+	approveKubernetesCSR(createdCSR)
+
+	result, err := csrAPI.UpdateApproval(createdCSR)
+	if err != nil {
+		return nil, err
+	}
+
+	return csrutils.WaitForCertificate(csrAPI, result, time.Second*10)
+}
+
+func generateKubernetesConfig(w io.Writer, server string, cert, key, ca []byte) {
+	newConfig := &api.Config{
+		CurrentContext: "cluster",
+		Clusters: []api.NamedCluster{
+			api.NamedCluster{
+				Name: "cluster",
+				Cluster: api.Cluster{
+					Server:                   server,
+					CertificateAuthorityData: ca,
+				},
+			},
+		},
+		Contexts: []api.NamedContext{
+			api.NamedContext{
+				Name: "cluster",
+				Context: api.Context{
+					Cluster:  "cluster",
+					AuthInfo: "user",
+				},
+			},
+		},
+		AuthInfos: []api.NamedAuthInfo{
+			api.NamedAuthInfo{
+				Name: "user",
+				AuthInfo: api.AuthInfo{
+					ClientCertificateData: cert,
+					ClientKeyData:         key,
+				},
+			},
+		},
+	}
+
+	encoder := json.NewYAMLSerializer(json.DefaultMetaFactory, nil, nil)
+	if err := encoder.Encode(newConfig, w); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func issueCertificate(csrAPI certificates.CertificateSigningRequestInterface, w io.Writer, subject, server string, ca []byte) error {
+	if subject == "" {
+		return fmt.Errorf("subject is empty")
+	}
+
+	if server == "" {
+		return fmt.Errorf("API server is not set")
+	}
+
+	key, csr, err := generateCSR([]string{subject})
+	if err != nil {
+		return err
+	}
+
+	cert, err := getCertificate(csrAPI, subject, csr)
+	if err != nil {
+		return err
+	}
+
+	generateKubernetesConfig(w, server, cert, key, ca)
+	return nil
+}
+
+func main() {
+	port := flag.Int("bind-port", 0, "port to bind to.")
+	addr := flag.String("bind-addr", "", "address to bind to.")
+	server := flag.String("api-server", os.Getenv("API_SERVER"), "API server address.")
+	subject := flag.String("subject", "", "subject to issue in cli mode")
+
+	flag.Parse()
+
+	cfg := config.GetConfigOrDie()
+
+	if err := rest.LoadTLSFiles(cfg); err != nil {
+		log.Fatal("error loading TLS certificates", err)
+	}
+
+	ca := cfg.TLSClientConfig.CAData
+
+	clientset, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		log.Fatal("error getting kubernetes client", err)
+	}
+
+	csrAPI := clientset.Certificates().CertificateSigningRequests()
+
+	if *port == 0 {
+		if err := issueCertificate(csrAPI, os.Stdout, *subject, *server, ca); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Disposition", "attachment; filename=kubeconfig.yaml")
+
+			user := r.Header.Get("X-Auth-User")
+
+			if err := issueCertificate(csrAPI, w, user, *server, ca); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+			log.Println("Issued certificate for", user)
+		})
+
+		log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", *addr, *port), nil))
+	}
+}
